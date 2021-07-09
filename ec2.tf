4d18813ab5aad1d31c448663be7e0ad315b1d679
Rockbesst Win <rockbesst@gmail.com>
2021-07-09 11:59:02 +0300

    restruct

---
+++ ec2.tf
@@ -0,0 +1,26 @@
+resource "aws_instance" "WebServer1" {
+	ami = data.aws_ami.amazon_linux.id
+	instance_type = var.instance_type
+	vpc_security_group_ids = [aws_security_group.asg_fra.id]
+	key_name = var.ssh_key
+	associate_public_ip_address = var.allow_public_ip
+	tags = merge(var.tags, {Name = "WebServer1"})
+}
+
+resource "aws_instance" "WebServer2" {
+	provider = aws.ire
+	ami = data.aws_ami.amazon_linux.id
+	instance_type = var.instance_type
+	vpc_security_group_ids = [aws_security_group.asg_ir.id]
+	associate_public_ip_address = var.allow_public_ip
+	tags = merge(var.tags, {Name = "WebServer2"})
+}
+# AMI Search
+data "aws_ami" "amazon_linux" {
+	owners = ["amazon"]
+	most_recent = true
+	filter {
+		name = "name"
+		values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+	}
+} 
