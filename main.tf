89aaa3fd7be4d1888c422531850e9e68a1ef1d25
Rockbesst Win <rockbesst@gmail.com>
2021-07-10 17:20:44 +0300

    fix

--- main.tf
+++ main.tf
@@ -77,13 +77,13 @@ resource "aws_security_group" "asg_ire" {
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
-    cidr_blocks      = [aws_vpc.vpc_ire.cidr_block]
+    cidr_blocks      = ["0.0.0.0/0"]
   }
   egress {
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
-    cidr_blocks      = [aws_vpc.vpc_ire.cidr_block]
+    cidr_blocks      = ["0.0.0.0/0"]
   }
   tags = {
     Name = "SG for Ireland region"
