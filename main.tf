d45007bd2b46069dea931de1cb44a511fa26139f
Rockbesst Win <rockbesst@gmail.com>
2021-07-10 17:12:12 +0300

    fix

--- main.tf
+++ main.tf
@@ -59,7 +59,7 @@ resource "aws_security_group" "asg_fra" {
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
-    cidr_blocks      = "0.0.0.0/0"
+    cidr_blocks      = ["0.0.0.0/0"]
   }
   tags = {
     Name = "SG for Frankfurt region"
